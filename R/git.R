99d4aa6389c2a3a6659c7926032ec1cdab344946
Jacob Malcom <jmalcom@defenders.org>
2016-06-23 16:49:17 -0400

    The basic commands are added.

--- R/git.R
+++ R/git.R
@@ -1,2 +1,56 @@
-Gqa <- system("git add --all")
+#' \code{git add --all}, aka, quick-add
+#'
+#' @return Any results from running \code{git add --all}
+#' @export
+#' @examples
+#' # Gqa()
+Gqa <- function() {
+  system("git add --all")
+}
 
+#' \code{git push} to a remote repo
+#'
+#' @return Any results from running the command
+#' @export
+#' @examples
+#' # Gpush()
+Gpush <- function() {
+  system("git push")
+}
+
+#' \code{git pull} from a remote repo
+#'
+#' @return Any results from running \code{git pull}
+#' @export
+#' @examples
+#' # Gpull()
+Gpush <- function() {
+  system("git pull")
+}
+
+#' \code{git commit -m 'x'} with message x
+#'
+#' @param msg A message to include with a commit
+#' @return Any results from running commit
+#' @export
+#' @examples
+#' # Gcommit("A silly commit message.")
+Gcommit <- function(msg) {
+  if(missing(msg)) stop("A message is needed for commit.")
+  cmd <- paste("git commit -m '", msg, "'")
+  system(cmd, wait = TRUE)
+}
+
+#' An empty \code{git} call
+#'
+#' To be used if a git command doesn't already exist in this package.
+#'
+#' @param cmd A message to include with a commit
+#' @return Any results from running the command
+#' @export
+#' @examples
+#' # git("add .")
+Git <- function(cmd = "") {
+  command <- paste("git", cmd)
+  system(command, wait = TRUE)
+}
